d9638c3fa18b813e6ca9e403dd44d42e78093cf7
CCameron2 <ccameron2@outlook.com>
2022-10-27 18:05:16 +0100

    Basic start to procgen (Needs much more work)

---
+++ Source/TeamProject/Land.cpp
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Land.h"
+#include "KismetProceduralMeshLibrary.h"
+
+// Sets default values
+ALand::ALand()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	
+	ProcMesh = CreateDefaultSubobject<UProceduralMeshComponent>(TEXT("Land Mesh"));
+	SetRootComponent(ProcMesh);
+}
+
+// Called when the game starts or when spawned
+void ALand::BeginPlay()
+{
+	Super::BeginPlay();
+
+	Vertices.Init({ 0,0,0 }, Size * Size);
+
+	for (int i = 0; i < Size; i++)
+	{
+		for (int j = 0; j < Size; j++)
+		{
+			FVector newVector = FVector{ i * Scale,j * Scale,0 };
+			Vertices[Size * i + j] = newVector;
+		}
+	}
+	UKismetProceduralMeshLibrary::CreateGridMeshTriangles(Size, Size, false, Triangles);
+
+	for (auto& vertex : Vertices)
+	{
+		vertex.Z = 1000 * FMath::PerlinNoise2D(FVector2D(vertex.X / 1000, vertex.Y / 1000));
+	}
+
+	//UKismetProceduralMeshLibrary::CalculateTangentsForMesh(Vertices, Triangles, UVs, Normals, Tangents);
+	ProcMesh->CreateMeshSection(0, Vertices, Triangles, Normals, UVs, VertexColours, Tangents, true);
+}
+
+// Called every frame
+void ALand::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/TeamProject/Land.h
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include <ProceduralMeshComponent.h>
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Land.generated.h"
+
+UCLASS()
+class TEAMPROJECT_API ALand : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ALand();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(VisibleAnywhere, Category = "Landmass")
+		UProceduralMeshComponent* ProcMesh;
+
+	UPROPERTY(EditAnywhere, Category = "Landmass")
+		int32 Size = 500;
+
+	UPROPERTY(EditAnywhere, Category = "Landmass")
+		float Scale = 50;
+
+
+	TArray<FVector> Vertices;
+	TArray<int32> Triangles;
+	TArray<FVector> Normals;
+	TArray<FVector2D> UVs;
+	TArray<FVector2D> UV1s;
+	TArray<FColor> VertexColours;
+	TArray<FProcMeshTangent> Tangents;
+
+
+
+};
