6dafafb000f4eff181595ac16b2d1a7164edd70b
Kielan Todd <kielantodd@hotmail.com>
2022-12-12 13:52:49 +0000

    AI Working

--- Source/TeamProject/MainAIController.cpp
+++ Source/TeamProject/MainAIController.cpp
@@ -2,13 +2,18 @@
 
 
 #include "MainAIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Kismet/GameplayStatics.h"
 
 void AMainAIController::BeginPlay()
 {
+	Super::BeginPlay();
 	if (AIBehaviour != nullptr)
 		RunBehaviorTree(AIBehaviour);
 }
 
 void AMainAIController::Tick(float DeltaTime)
 {
+	Super::Tick(DeltaTime);
+	UE_LOG(LogTemp, Warning, TEXT("AI Controller ticking"));
 }
